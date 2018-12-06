fe125a075053f065dcf8f02f38007f2e83a5f36e
inux <inux.steve@gmail.com>
2018-12-06 15:28:27 +0100

    add R project - update documentation

---
+++ datasetMerge/datasetMerger.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(utils)
+
+dfAppleStore <- read.csv("../resources/AppleStore.csv")
+dfPlayStore <- read.csv("../resources/googleplaystore.csv")
+
+View(dfAppleStore)
+View(dfPlayStore)
