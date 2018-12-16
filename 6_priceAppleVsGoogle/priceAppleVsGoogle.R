d2f9c1555da1bce0f6ad774d9fee47f3541def91
inux <inux.steve@gmail.com>
2018-12-16 21:42:19 +0100

    remove unused code

--- 6_priceAppleVsGoogle/priceAppleVsGoogle.R
+++ 6_priceAppleVsGoogle/priceAppleVsGoogle.R
@@ -8,16 +8,9 @@ library(gridExtra)
 library(grid)
 
 df <- read.csv("../resources/_dfAppleGoogle_merged.csv", header = TRUE, encoding = "UTF-8", stringsAsFactors = FALSE)
-View(df)
+#View(df)
 
-#p1 <- ggplot(data = df, mapping = aes(x = category.y, y = price.y, color = source.y)) + coord_flip() + geom_point()
-#print(p1)
-#p2 <- ggplot(data = df, mapping = aes(x = category.y, y = price.x, color = source.x)) + coord_flip() + geom_point()
-#print(p2)
-
-#grid.arrange(p1, p2)
-
-
+#Zwei Boxplots
 papple <- ggplot(df, aes(x=category.x, y=price.x)) +
   geom_bar(position="dodge", colour="blue", stat = "identity")
 
@@ -25,4 +18,3 @@ pgoogle <- ggplot(df, aes(x=category.y, y=price.y)) +
   geom_bar(position="dodge", colour="red", stat = "identity")
 
 grid.arrange(papple, pgoogle)
-
